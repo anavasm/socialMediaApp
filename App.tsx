587ca1dfe19e5942f88166e3fd747d39e7e3e18a
Alvaro Navas Mesa <alvaronavasmesa@gmail.com>
2026-08-27 13:34:24 +0200

    feat(header): add explore title with message icon and notification badge

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, TouchableOpacity, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import Title from './components/Title';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { getFontFamily } from './assets/fonts/helper';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -26,7 +29,15 @@ function AppContent() {
           paddingBottom: insets.bottom,
         },
       ]}>
-      <Title>Let's Explore</Title>
+        <View style={styles.header} >
+          <Title>Let's Explore</Title>
+          <TouchableOpacity style={styles.iconContainer}>
+            <FontAwesomeIcon icon={faEnvelope} size={20} color="#022150" />
+            <View style={styles.messageNumberContainer}>
+              <Text style={styles.messageNumber}>2</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -34,7 +45,37 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 27,
+    marginRight: 17,
+    marginTop: 30,
+  },
+  iconContainer: {
+    padding: 14,
+    backgroundColor: '#F9FAFB',
+    borderRadius: 100,
+  },
+  messageNumberContainer: {
+    backgroundColor: '#F35BAC',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 14,
+    height: 14,
+    borderRadius: 7,
+    position: 'absolute',
+    right: 8,
+    top: 8,
+  },
+  messageNumber: {
+    color: '#FFFFFF',
+    fontSize: 8,
+    fontFamily: getFontFamily('Inter', '600'),
+  },
+
 });
 
 export default App;
